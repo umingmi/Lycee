64339263e84b8e44c343ee6005a080d573d0c758
umi <lumanog.dev@gmail.com>
2024-05-18 14:08:30 +0800

    Added: Navbar

--- index.php
+++ index.php
@@ -1,16 +1,36 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>Lycee</title>
+        <!--============== META ===============-->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Lycee</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="styles.css">
-    </head>
-    <body>
-        
 
-   
+        <!--============== CSS ===============-->
+        <link rel="stylesheet" href="styles.css">
+
+        <!--============== REMIX ICON ===============-->
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css"rel="stylesheet"/>
+        
+        <!--============== FONT ===============-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Changa+One:ital@0;1&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <!--============== HEADER ===============-->
+        <header class="header">
+            <a href="#" class="navlogo">
+                <i class="ri-outlet-fill"></i><span>Lycee</span>
+            </a>
+            <ul class="navbar">
+                <li><a href="Login">Login</a></li>
+                <li><a href="Register"><button class="reg-btn">Register</button></a></li>
+            </ul>
+        </header>
     </body>
 </html>
